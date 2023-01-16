89ddfd22c5c672f95d53aa64f87dd96ac2585087
Bartosz Wajs <bartezzwajs@gmail.com>
2023-01-16 23:22:14 +0100

    Implementation of CD command (to be completed)

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 
 void command(char *input, char *parameters[]){
 	if(fgets(input, 1024, stdin) != NULL){
+		int length = strlen(input) - 1;
+		input[length] = 0;
 		char* parameter = strtok(input, " ");
 		for(int i = 0; parameter != NULL; i++){
 			parameters[i] = parameter;
@@ -19,20 +21,36 @@ void help(){
 	printf("Available commands:\ncd, help, exit\n");
 }
 
+void cd(char* parameter){
+	if(parameter[0] == '~'){
+		chdir(getenv("HOME"));
+	}else if(chdir(parameter) != 0){
+		printf("No such file or directory\n");
+	}
+}
 
 int main(int argc, char *argv[]) {
     char path[1024];
     char input[1024];
     char* parameters[1024];
-    getcwd(path, sizeof(path));
     while(1){
+	getcwd(path, sizeof(path));
 	printf("%s@%s$ ", getenv("USER"), path);
 	command(input, parameters);
-	if(strcmp("exit\n", parameters[0]) == 0){
+	if(strcmp("exit", parameters[0]) == 0){
 		exit(0);
-	}else if(strcmp("help\n", parameters[0]) == 0){
+	}else if(strcmp("help", parameters[0]) == 0){
 		help();
-	}	
+	}else if(strcmp("cd", parameters[0]) == 0){
+		if(parameters[1] == NULL){
+			chdir(getenv("HOME"));
+		}else{
+			cd(parameters[1]);
+		}
+	}else{
+		printf("Unknown command\n");
+	}
+
 
 		
 		
