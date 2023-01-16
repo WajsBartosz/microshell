493f590a93fe4c2e686886d80095e95ac5638231
Bartosz Wajs <bartezzwajs@gmail.com>
2023-01-16 18:08:12 +0100

    applied first version of program that shows only path

--- main.c
+++ main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-int main() {
+int main(int argc, char *argv[]) {
+    char path[1024];
+    getcwd(path, sizeof(path));
+    printf("%s$ ",path);
 
+    return 0;
 }
