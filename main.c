39e90dac40d88b9e55acd4819ef9a11412f35ee6
Bartosz Wajs <bartezzwajs@gmail.com>
2023-01-17 19:53:44 +0100

    repaired cd function that works now and implemented fork() and execvp() functions to handle other linux commands

--- main.c
+++ main.c
@@ -2,16 +2,20 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
 
 void command(char *input, char *parameters[]){
 	if(fgets(input, 1024, stdin) != NULL){
 		int length = strlen(input) - 1;
 		input[length] = 0;
+		int wordCounter = 0;
 		char* parameter = strtok(input, " ");
 		for(int i = 0; parameter != NULL; i++){
 			parameters[i] = parameter;
 			parameter = strtok(NULL, " ");
+			wordCounter++;
 		}
+		parameters[wordCounter] = NULL;
 	}
 }
 
@@ -25,7 +29,7 @@ void cd(char* parameter){
 	if(parameter[0] == '~'){
 		chdir(getenv("HOME"));
 	}else if(chdir(parameter) != 0){
-		printf("No such file or directory\n");
+		perror("No such file or directory\n");
 	}
 }
 
@@ -35,7 +39,9 @@ int main(int argc, char *argv[]) {
     char* parameters[1024];
     while(1){
 	getcwd(path, sizeof(path));
-	printf("%s@%s$ ", getenv("USER"), path);
+	printf("\033[1;31m");
+	printf("%s:%s$ ", getenv("USER"), path);
+	printf("\033[0m");
 	command(input, parameters);
 	if(strcmp("exit", parameters[0]) == 0){
 		exit(0);
@@ -48,7 +54,16 @@ int main(int argc, char *argv[]) {
 			cd(parameters[1]);
 		}
 	}else{
-		printf("Unknown command\n");
+		int pid = fork();
+		if(pid == 0){
+			if(execvp(parameters[0], parameters) == -1){
+				perror("Unknown command\n");
+			}else{
+				exit(0);
+			}
+		}else{
+			wait(NULL);
+		}
 	}
 
 
