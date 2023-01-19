edcf01af4b15d02d743ed12301ee5427bda6ffbf
WajsBartosz <70707076+WajsBartosz@users.noreply.github.com>
2023-01-19 19:31:36 +0100

    Implementation of ls command (FINAL VERSION)

--- main.c
+++ main.c
@@ -63,22 +63,78 @@ int command(char * input, char * parameters[]) {
 
 void help() {
   printf("Microshell\nCreated by Wajs Bartosz\n");
-  printf("Available commands:\ncd, help, exit\n");
+  printf("Own implementation of commands:\ncd, help, exit, ls and touch\n");
+  printf("Other commands are implemented using ");
 }
 
-/*void ls(char *parameter){
+void cd(int size, char *parameters[]){
+  if (parameters[1] == NULL || strcmp("~", parameters[1]) == 0) {
+    chdir(getenv("HOME"));
+  } else if (size > 2) {
+    puts("-bash: cd: too many arguments");
+  } else if (chdir(parameters[1]) != 0) {
+    char error[1024];
+    sprintf(error, "-bash: cd: %s", parameters[1]);
+    perror(error);
+  }
+}
+
+void ls(int size, char *parameters[]){
 	DIR *openDirectory;
-	openDirectory = opendir(parameter[0]);
+  struct dirent *entry;
+  char path[1024] = "";
+  char isASet = 0;
+  int IndexA = 1;
+  for(int i = 1; i < size; i++){
+    if(strcmp("-a", parameters[i]) == 0){
+      isASet = 1;
+      break;
+    }else{
+      IndexA++;
+    }
+  }
+  if(size == 1 || (size == 2 && isASet == 1)){
+    getcwd(path, sizeof(path));
+  } else if(size == 2 && isASet == 0){
+    strcpy(path, parameters[1]);
+  }else if(size == 3 && isASet == 1){
+    strcpy(path, parameters[3-IndexA]);
+  }else{
+    puts("Too many arguments");
+    return;
+  }
+  openDirectory = opendir(path);
 	if(openDirectory == NULL){
-		perror("Error");
+		puts("Unable to find directory");
 	}else{
-		while(entry = readdir(openDirectory)){
-			printf("%s", entry->d_name);
+		while((entry = readdir(openDirectory)) != NULL){
+      if(isASet == 1){
+        printf("%s   ", entry->d_name);
+      }else{
+        if(entry->d_name[0] != '.'){
+          printf("%s   ", entry->d_name);
+        }
+      }
 		}
 		printf("\n");
-		closedir(parameter[0]);
+		closedir(openDirectory);
 	}
-}*/
+}
+
+void touch(int size, char *parameters[]){
+  FILE * fPtr;
+    for (int i = 1; i < size; i++) {
+      char name[1024];
+      strcpy(name, parameters[i]);
+      fPtr = fopen(name, "w");
+      if (fPtr == NULL) {
+        printf("Unable to create file");
+      } else {
+        fclose(fPtr);
+      }
+    }
+}
+
 int main(int argc, char * argv[]) {
   char path[1024];
   char input[1024];
@@ -96,29 +152,12 @@ int main(int argc, char * argv[]) {
       } else if (strcmp("help", parameters[0]) == 0) {
         help();
       } else if (strcmp("cd", parameters[0]) == 0) {
-        if (parameters[1] == NULL || strcmp("~", parameters[1]) == 0) {
-          chdir(getenv("HOME"));
-        } else if (size > 2) {
-          puts("-bash: cd: too many arguments");
-        } else if (chdir(parameters[1]) != 0) {
-          char error[1024];
-          sprintf(error, "-bash: cd: %s", parameters[1]);
-          perror(error);
-        }
+        cd(size, parameters);
       } else if (strcmp("ls", parameters[0]) == 0) {
-        //ls(parameters);
+        ls(size, parameters);
       } else if (strcmp("touch", parameters[0]) == 0) {
-        FILE * fPtr;
-        for (int i = 1; i < size; i++) {
-          char * name = parameters[i];
-          fPtr = fopen(name, "w");
-          if (fPtr == NULL) {
-            printf("Unable to create file");
-          } else {
-            fclose(fPtr);
-          }
-        }
-      } else if (strcmp("history", parameters[0]) == 0){
+        touch(size, parameters);
+      } else if (strcmp("history", parameters[0]) == 0) {
         history();
       } else {
         int pid = fork();
